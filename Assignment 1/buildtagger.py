61d965f4c8bd150230e65c0ce7dfaf271e5bc7ac
Aadit Kamat <aadit.k12@gmail.com>
2020-09-26 00:03:18 +0800

    Process word and pos tag counts with dictionaries

--- Assignment 1/buildtagger.py
+++ Assignment 1/buildtagger.py
@@ -20,34 +20,44 @@ def calculate_word_pos_counts(word_pos_counts, current_word, current_pos_tag):
         word_pos_counts[current_word][current_pos_tag] = 1
     else:
         word_pos_counts[current_word] = {current_pos_tag: 1}
-    
+
+def calculate_unigram_counts(token, counts):
+    if token in counts:
+        counts[token] += 1
+    else:
+        counts[token] = 1
+
 def custom_split(tagged_word):
     return ('/'.join(tagged_word.split('/')[0: -1]), tagged_word.split('/')[-1])
 
-def process_lines(pos_pos_bigram_counts, word_pos_counts, pos_tags, lines):
+def process_lines(pos_pos_bigram_counts, word_pos_counts, word_counts, pos_tag_counts, lines):
     for line in lines:
         tagged_words = line.split(' ')
         current_word, current_pos_tag = custom_split(tagged_words[0])
         calculate_word_pos_counts(word_pos_counts, current_word, current_pos_tag)
-        pos_tags.add(current_pos_tag)
+        calculate_unigram_counts(current_word, word_counts)
+        calculate_unigram_counts(current_pos_tag, pos_tag_counts)
         for i in range(1, len(tagged_words)):
             current_word, current_pos_tag = custom_split(tagged_words[i])
             previous_word, previous_pos_tag = custom_split(tagged_words[i - 1])
             calculate_pos_pos_bigram_counts(pos_pos_bigram_counts, previous_pos_tag, current_pos_tag)
             calculate_word_pos_counts(word_pos_counts, current_word, current_pos_tag)
-            pos_tags.add(current_pos_tag)
+            calculate_unigram_counts(current_word, word_counts)
+            calculate_unigram_counts(current_pos_tag, pos_tag_counts)
 
 def train_model(train_file, model_file):
     # write your code here. You can add functions as well.
     with open(train_file) as file:
         pos_pos_bigram_counts = {}
         word_pos_counts = {}
-        pos_tags = set()
+        word_counts = {}
+        pos_tag_counts = {}
         train_data = file.read()
         lines = train_data.split('\n')
-        process_lines(pos_pos_bigram_counts, word_pos_counts, pos_tags, lines)
+        process_lines(pos_pos_bigram_counts, word_pos_counts, word_counts, pos_tag_counts, lines)
         print('Debugging info: ')
-        print(f'pos pos bigram counts: {pos_pos_bigram_counts}\n word pos bigram counts:{word_pos_counts}\n pos_tags:{pos_tags}')
+        print(f'pos pos bigram counts: {pos_pos_bigram_counts}\n word pos bigram counts:{word_pos_counts}\n pos_tags:{pos_tag_counts} \n word_counts:{word_counts}')
+
 
 if __name__ == "__main__":
     # make no changes here
