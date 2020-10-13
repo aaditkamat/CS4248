47aca0b205e102975e55c66ace61a7dd0a5307a4
Aadit Kamat <aadit.k12@gmail.com>
2020-10-13 17:29:53 +0800

    Simplify code with better data representations

--- Assignment 1/buildtagger.py
+++ Assignment 1/buildtagger.py
@@ -6,104 +6,81 @@ import math
 import sys
 import datetime
 
+from collections import defaultdict
+from itertools import permutations, product
+
 START_TOKEN = '<s>'
 END_TOKEN = '</s>'
 
 class HiddenMarkovModel():
-    def __init__(self, pos_pos_bigram_counts, pos_unigram_counts, word_unigram_counts, word_pos_bigram_counts):
-        self.transition_probabilities = self.calculate_transition_probabilities(pos_pos_bigram_counts, pos_unigram_counts)
-        self.observation_likelihoods = self.calculate_observation_likelihoods(word_unigram_counts, word_pos_bigram_counts, pos_unigram_counts)
+    def __init__(self, word_counts, tag_counts, bigram_counts):
+        self.transition_probs, self.emission_probs = defaultdict(float), defaultdict(float) 
+        self.transition_probs = self.calculate_transition_probs(tag_counts, bigram_counts)
+        self.emission_probs = self.calculate_emission_probs(word_counts, tag_counts, bigram_counts)
 
-    def calculate_transition_probabilities(self, pos_pos_bigram_counts, pos_unigram_counts):
-        pos_tags = set(pos_unigram_counts.keys())
-        probabilities = {}
-        for first_tag in pos_tags:
-            probabilities[first_tag] = {}
-            for second_tag in pos_tags:
-                if first_tag in pos_pos_bigram_counts and second_tag in pos_pos_bigram_counts[first_tag]:
-                    probabilities[first_tag][second_tag] = pos_pos_bigram_counts[first_tag][second_tag] / pos_unigram_counts[first_tag]
-                else:
-                    probabilities[first_tag][second_tag] = 0
-        return probabilities
+    def calculate_transition_probs(self, tag_counts, bigram_counts):
+        tags = list(tag_counts.keys())
+        bigrams = permutations(tags, 2)
+        return {bigram: bigram_counts[bigram] / tag_counts[bigram[0]] if tag_counts[bigram[0]] > 0 else 0 for bigram in bigrams}
 
-    def calculate_observation_likelihoods(self, word_unigram_counts, word_pos_bigram_counts, pos_unigram_counts):
-        pos_tags = set(pos_unigram_counts.keys())
-        # actual words in the training file
-        words = set(word_unigram_counts.keys()) - set([START_TOKEN, END_TOKEN])
-        probabilities = {}
-        for word in words:
-            probabilities[word] = {}
-            for pos_tag in pos_tags:
-                if word in word_pos_bigram_counts and pos_tag in word_pos_bigram_counts[word]:
-                    probabilities[word][pos_tag] = word_pos_bigram_counts[word][pos_tag] / word_unigram_counts[word]
-                else:
-                    probabilities[word][pos_tag] = 0
-        return probabilities
-        
-def calculate_bigram_counts(bigram_counts, previous_token, current_token):
-    if previous_token in bigram_counts and current_token in bigram_counts[previous_token]:
-        bigram_counts[previous_token][current_token] += 1
-    elif previous_token in bigram_counts:
-        bigram_counts[previous_token][current_token] = 1
-    else:
-        bigram_counts[previous_token] = {current_token: 1}
+    def calculate_emission_probs(self, word_counts, tag_counts, bigram_counts):
+        words = list(word_counts.keys())
+        tags = list(tag_counts.keys())
+        bigrams = product(tags, words)
+        return {bigram: bigram_counts[bigram] / tag_counts[bigram[0]] if tag_counts[bigram[0]] > 0 else 0 for bigram in bigrams }
 
-def calculate_pos_pos_bigram_counts(pos_pos_bigram_counts, previous_pos, current_pos):
-    calculate_bigram_counts(pos_pos_bigram_counts, previous_pos, current_pos)
-
-def calculate_word_pos_bigram_counts(word_pos_bigram_counts, current_word, current_pos):
-    calculate_bigram_counts(word_pos_bigram_counts, current_word, current_pos)
-
-def calculate_unigram_counts(token, counts):
-    if token in counts:
-        counts[token] += 1
-    else:
-        counts[token] = 1
-
-def custom_split(tagged_word):
-    return ('/'.join(tagged_word.split('/')[0: -1]), tagged_word.split('/')[-1])
+def get_word_tag_pairs(line):
+    return [('/'.join(token.split('/')[0: -1]), token.split('/')[-1]) for token in line.split(' ')]
 
 def process_train_file(train_file):
     with open(train_file, 'r') as train_file_handler:
-        pos_pos_bigram_counts, word_pos_bigram_counts, word_unigram_counts, pos_unigram_counts = {}, {}, {}, {}
+        bigram_counts, word_counts, tag_counts = defaultdict(int), defaultdict(int),  defaultdict(int)
         train_data = train_file_handler.read()
         lines = train_data.split('\n')
+
         for line in lines:
-            tagged_words = line.split(' ')
-            calculate_unigram_counts(START_TOKEN, word_unigram_counts)
-            calculate_unigram_counts(START_TOKEN, pos_unigram_counts)
-            for i in range(len(tagged_words)):
-                current_word, current_pos = custom_split(tagged_words[i])
-                calculate_word_pos_bigram_counts(word_pos_bigram_counts, current_word, current_pos)
-                calculate_unigram_counts(current_word, word_unigram_counts)
-                calculate_unigram_counts(current_pos, pos_unigram_counts)
-                # use start token at the beginning of a sentence as a pos tag
+            word_tag_pairs = get_word_tag_pairs(line)
+
+            for i in range(len(word_tag_pairs)):
+                current_word, current_tag = word_tag_pairs[i]
+                word_counts[current_word] += 1
+                tag_counts[current_tag] += 1
+
+                bigram_counts[(current_tag, current_word)] += 1
+                # handle special start token seperately
                 if i == 0:
-                    calculate_pos_pos_bigram_counts(pos_pos_bigram_counts, START_TOKEN, current_pos)
-                    calculate_word_pos_bigram_counts(word_pos_bigram_counts, current_word, START_TOKEN)
+                    bigram_counts[(START_TOKEN, current_tag)] += 1
+                    bigram_counts[(current_word, START_TOKEN)] += 1
                 else:
-                    previous_word, previous_pos = custom_split(tagged_words[i - 1])
-                    calculate_pos_pos_bigram_counts(pos_pos_bigram_counts, previous_pos, current_pos)
-            calculate_unigram_counts(END_TOKEN, word_unigram_counts)
-            calculate_unigram_counts(END_TOKEN, pos_unigram_counts)
-            calculate_pos_pos_bigram_counts(pos_pos_bigram_counts, current_pos, END_TOKEN)
-        return pos_pos_bigram_counts, word_pos_bigram_counts, word_unigram_counts, pos_unigram_counts
+                    previous_word, previous_tag = word_tag_pairs[i - 1]
+                    bigram_counts[(previous_tag, current_tag)] += 1
+
+            # handle special end token seperately
+            bigram_counts[(current_tag, END_TOKEN)] += 1
+
+        # For convenience in calculating probabilities
+        tag_counts[START_TOKEN] = len(lines)
+        tag_counts[END_TOKEN] = len(lines)
+        return word_counts, tag_counts, bigram_counts
 
 def write_to_model_file(model_file, model):
     with open(model_file, mode='wb') as model_file_handler:
             pickle.dump(model, model_file_handler)
 
 def train_model(train_file, model_file):
-    pos_pos_bigram_counts, word_pos_bigram_counts, word_unigram_counts, pos_unigram_counts = process_train_file(train_file)
-    hmm = HiddenMarkovModel(pos_pos_bigram_counts, pos_unigram_counts, word_unigram_counts, word_pos_bigram_counts)
+    word_counts, tag_counts, bigram_counts = process_train_file(train_file)
+    hmm = HiddenMarkovModel(word_counts, tag_counts, bigram_counts)
+
+    # Remove after calculating probabilities
+    del tag_counts[START_TOKEN]
+    del tag_counts[END_TOKEN]
+
     model = {
-        'pos_tags': list(set(pos_unigram_counts.keys()) - set([START_TOKEN, END_TOKEN])),
-        'word_unigram_counts': word_unigram_counts,
-        'pos_unigram_counts': pos_unigram_counts,
-        'pos_pos_bigram_counts': pos_pos_bigram_counts,
-        'word_pos_bigram_counts': word_pos_bigram_counts,
-        'transition_probabilities': hmm.transition_probabilities,
-        'observation_likelihoods': hmm.observation_likelihoods
+        'word_counts': word_counts,
+        'tag_counts': tag_counts,
+        'bigram_counts': bigram_counts,
+        'transition_probs': hmm.transition_probs,
+        'emission_probs': hmm.emission_probs
     }
     # pdb.set_trace()
     write_to_model_file(model_file, model)
--- Assignment 1/runtagger.py
+++ Assignment 1/runtagger.py
@@ -6,87 +6,89 @@ import math
 import sys
 import datetime
 
+from collections import defaultdict
+
 START_TOKEN = '<s>'
 END_TOKEN = '</s>'
 
-# Set probability of OOV (Out of Vocabulary) words as 0
-def handle_out_of_vocabulary(lines, model):
-    words = set([word for line in lines for word in line.split(' ')])
-    pos_tags = model['pos_tags']
-    observation_likelihoods = model['observation_likelihoods']
-    for word in words:
-        if word not in observation_likelihoods:
-            observation_likelihoods[word] = {}
-            for pos_tag in pos_tags:
-                observation_likelihoods[word][pos_tag] = 0
+# Set prob of OOV (Out of Vocabulary) words as 0
+# def handle_out_of_vocabulary(lines, model):
+#     words = set([word for line in lines for word in line.split(' ')])
+#     pos_tags = model['pos_tags']
+#     emission_probs = model['emission_probs']
+#     for word in words:
+#         if word not in emission_probs:
+#             emission_probs[word] = {}
+#             for pos_tag in pos_tags:
+#                 emission_probs[word][pos_tag] = 0
 
-def calculate_alpha_values(bigram_counts, unigram_probabilities, bigram_probabilities):
+def calculate_alpha_values(bigram_counts, unigram_probs, bigram_probs):
     alpha_values = {}
     for prev_token in bigram_counts:
-        numerator = 1 - sum([bigram_probabilities[prev_token][next_token] for next_token in bigram_counts[prev_token] if bigram_counts[prev_token][next_token] > 0])
-        denominator = sum([unigram_probabilities[next_token] for next_token in bigram_counts[prev_token] if bigram_counts[prev_token][next_token] == 0])
+        numerator = 1 - sum([bigram_probs[prev_token][next_token] for next_token in bigram_counts[prev_token] if bigram_counts[prev_token][next_token] > 0])
+        denominator = sum([unigram_probs[next_token] for next_token in bigram_counts[prev_token] if bigram_counts[prev_token][next_token] == 0])
         alpha_values[prev_token] = numerator/denominator if denominator > 0 else numerator
     return alpha_values
 
-def discount(probabilities, additive=0.01):
-    # Unigram probabilities
-    if type(list(probabilities.values())[0]) == float:
-        unigram_probabilities = probabilities
-        count_zeroes = len([probability for probability in unigram_probabilities.values() if probability == 0 ])
-        discount_factor = additive * count_zeroes / (len(unigram_probabilities.values()) - count_zeroes)
-        return {unigram: probability + additive if probability == 0 else probability  - discount_factor for unigram, probability in unigram_probabilities.items()}
-    # Bigram probabilities
-    bigram_probabilities = probabilities
-    count_zeroes = len([bigram_probabilities[prev_token][next_token] for prev_token in bigram_probabilities for next_token in bigram_probabilities[prev_token] if bigram_probabilities[prev_token][next_token] == 0 ])
-    discount_factor = additive * count_zeroes / (sum([len(bigram_probabilities[prev_token].values()) for prev_token in bigram_probabilities]) - count_zeroes)
-    discounted_bigram_probabilities = {}
-    for prev_token in bigram_probabilities:
-        discounted_bigram_probabilities[prev_token] = {}
-        for next_token in bigram_probabilities[prev_token]:
-            probability = bigram_probabilities[prev_token][next_token]
-            new_probability = probability + additive if probability == 0 else abs(probability - discount_factor)
-            discounted_bigram_probabilities[prev_token][next_token] = new_probability
-    return discounted_bigram_probabilities
+def discount(probs, additive=0.01):
+    # Unigram probs
+    if type(list(probs.values())[0]) == float:
+        unigram_probs = probs
+        count_zeroes = len([prob for prob in unigram_probs.values() if prob == 0 ])
+        discount_factor = additive * count_zeroes / (len(unigram_probs.values()) - count_zeroes)
+        return {unigram: prob + additive if prob == 0 else prob  - discount_factor for unigram, prob in unigram_probs.items()}
+    # Bigram probs
+    bigram_probs = probs
+    count_zeroes = len([bigram_probs[prev_token][next_token] for prev_token in bigram_probs for next_token in bigram_probs[prev_token] if bigram_probs[prev_token][next_token] == 0 ])
+    discount_factor = additive * count_zeroes / (sum([len(bigram_probs[prev_token].values()) for prev_token in bigram_probs]) - count_zeroes)
+    discounted_bigram_probs = {}
+    for prev_token in bigram_probs:
+        discounted_bigram_probs[prev_token] = {}
+        for next_token in bigram_probs[prev_token]:
+            prob = bigram_probs[prev_token][next_token]
+            new_prob = prob + additive if prob == 0 else abs(prob - discount_factor)
+            discounted_bigram_probs[prev_token][next_token] = new_prob
+    return discounted_bigram_probs
 
-def smoothen(unigram_counts, bigram_counts, bigram_probabilities):
+def smoothen(unigram_counts, bigram_counts, bigram_probs):
     total_number_of_unigrams = sum(unigram_counts.values())
-    unigram_probabilities = {unigram: unigram_counts[unigram] / total_number_of_unigrams for unigram in unigram_counts}
-    discounted_unigram_probabilities = discount(unigram_probabilities)
-    discounted_bigram_probabilities = discount(bigram_probabilities)
+    unigram_probs = {unigram: unigram_counts[unigram] / total_number_of_unigrams for unigram in unigram_counts}
+    discounted_unigram_probs = discount(unigram_probs)
+    discounted_bigram_probs = discount(bigram_probs)
     pdb.set_trace()
-    alpha_values = calculate_alpha_values(bigram_counts, discounted_unigram_probabilities, discounted_bigram_probabilities)
+    alpha_values = calculate_alpha_values(bigram_counts, discounted_unigram_probs, discounted_bigram_probs)
     for prev_token in bigram_counts:
         for next_token in bigram_counts[prev_token]:
             if bigram_counts[prev_token][next_token] > 0:
-                bigram_probabilities[prev_token][next_token] = discounted_bigram_probabilities[prev_token][next_token]
+                bigram_probs[prev_token][next_token] = discounted_bigram_probs[prev_token][next_token]
             else:
-                bigram_probabilities[prev_token][next_token] = alpha_values[prev_token] * discounted_unigram_probabilities[next_token]
+                bigram_probs[prev_token][next_token] = alpha_values[prev_token] * discounted_unigram_probs[next_token]
 
-def preprocess(lines, model):
-    handle_out_of_vocabulary(lines, model)
-    word_unigram_counts, word_pos_bigram_counts, observation_likelihoods = model['word_unigram_counts'], model['word_pos_bigram_counts'], model['observation_likelihoods']
-    # smoothen(word_unigram_counts, word_pos_bigram_counts, observation_likelihoods)
-    pos_unigram_counts, pos_pos_bigram_counts, transition_probabilities = model['word_unigram_counts'], model['pos_pos_bigram_counts'], model['transition_probabilities']
-    # smoothen(pos_unigram_counts, pos_pos_bigram_counts, transition_probabilities)
+# def preprocess(lines, model):
+#     # handle_out_of_vocabulary(lines, model)
+#     word_unigram_counts, word_pos_bigram_counts, emission_probs = model['word_unigram_counts'], model['word_pos_bigram_counts'], model['emission_probs']
+#     # smoothen(word_unigram_counts, word_pos_bigram_counts, emission_probs)
+#     pos_unigram_counts, pos_pos_bigram_counts, transition_probs = model['word_unigram_counts'], model['pos_pos_bigram_counts'], model['transition_probs']
+#     # smoothen(pos_unigram_counts, pos_pos_bigram_counts, transition_probs)
 
 def viterbi(model, line):
     words = line.split(' ')
-    transition_probabilities = model['transition_probabilities']
-    observation_likelihoods = model['observation_likelihoods']
-    pos_tags = model['pos_tags']
-    forward_ptr = {}
+    transition_probs = model['transition_probs']
+    emission_probs = model['emission_probs']
+    tags = model['tag_counts'].keys()
+    forward_ptr = defaultdict(float)
 
-    for pos_tag in pos_tags:
-        forward_ptr[pos_tag] = {}
-        forward_ptr[pos_tag][words[0]] = transition_probabilities[START_TOKEN][pos_tag] * observation_likelihoods[words[0]][pos_tag]
+    for tag in tags:
+        forward_ptr[(tag, words[0])] = transition_probs[(START_TOKEN, tag)] * emission_probs[(tag, words[0])]
 
     for i in range(1, len(words)):
-        for curr_pos_tag in pos_tags:          
-            forward_ptr[curr_pos_tag][words[i]] = max([forward_ptr[prev_pos_tag][words[i - 1]] * transition_probabilities[prev_pos_tag][curr_pos_tag] * observation_likelihoods[words[i]][curr_pos_tag] for prev_pos_tag in pos_tags])
+        word1, word2 = words[i - 1: i + 1]
+        for tag2 in tags:          
+            forward_ptr[(tag2, word2)] = max([forward_ptr[(tag1, word1)] * transition_probs[(tag1, tag2)] * emission_probs[(tag2, word2)] for tag1 in tags])
 
-    forward_ptr[END_TOKEN] = {}
-    forward_ptr[END_TOKEN][words[-1]] = max([forward_ptr[pos_tag][words[-1]] * transition_probabilities[pos_tag][END_TOKEN] for pos_tag in pos_tags])
+    forward_ptr[(END_TOKEN, words[-1])] = max([forward_ptr[(tag, words[-1])] * transition_probs[(tag, END_TOKEN)] for tag in tags])
 
+    pdb.set_trace()
     return forward_ptr
 
 def read_test_file(test_file):
@@ -94,17 +96,30 @@ def read_test_file(test_file):
         lines = test_file_handler.read().split('\n')
         return lines
 
+# Objects obtained from the pickled file need to be converted to the custom defaultdict type 
+# because they are stored as dictionaries
+def restore_types(model):
+    typed_model = {}
+    for name, dictionary in model.items():
+        new_dictionary = defaultdict(type(list(dictionary.values())[0]))
+        for key in dictionary:
+            new_dictionary[key] = dictionary[key]
+        typed_model[name] = new_dictionary
+    return typed_model
+
 def read_model_file(model_file):
     with open(model_file, mode='rb') as model_file_handler:
-        return pickle.load(model_file_handler)
+        model = pickle.load(model_file_handler)
+        return restore_types(model)
 
-def get_pos_tags(words, pos_tags, forward_ptr):
+
+def get_pos_tags(words, tags, forward_ptr):
     generated_pos_tags = []
     for word in words:
-        best_tag, probability = pos_tags[0], forward_ptr[pos_tags[0]][word]
-        for tag in pos_tags[1: ]:
-            if forward_ptr[tag][word] > probability:
-                best_tag, probability = tag, forward_ptr[tag][word]
+        best_tag, prob = None, 0
+        for tag in tags:
+            if forward_ptr[(tag, word)] >= prob:
+                best_tag, prob = tag, forward_ptr[(tag, word)]
         generated_pos_tags.append(best_tag)
     return generated_pos_tags
 
@@ -115,9 +130,9 @@ def write_to_output_file(lines, out_file, model):
             forward_ptr = viterbi(model, line)
             words = line.split(' ')
 
-            pos_tags = model['pos_tags']
+            tags = model['tag_counts'].keys()
  
-            word_tags = get_pos_tags(words, pos_tags, forward_ptr)
+            word_tags = get_pos_tags(words, tags, forward_ptr)
             new_line = ' '.join(['{}/{}'.format(words[i], word_tags[i]) for i in range(len(words))])
             output_file_handler.write(new_line + '\n')
             ctr += 1
@@ -125,7 +140,7 @@ def write_to_output_file(lines, out_file, model):
 def tag_sentence(test_file, model_file, out_file, start_time):
     lines = read_test_file(test_file)
     model = read_model_file(model_file)
-    preprocess(lines, model)
+    # preprocess(lines, model)
     write_to_output_file(lines, out_file, model)
     
 if __name__ == "__main__":
